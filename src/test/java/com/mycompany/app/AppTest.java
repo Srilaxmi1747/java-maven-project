292d971db207e972952797ea9de0e4240a484d60
Srilaxmi1747 <srilaxmi.veeramalla@gmail.com>
2025-10-29 15:56:41 +0530

    my first commit

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
